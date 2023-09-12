889d79adb811d5fbb0884c14985db50a9ab21aae
didwldk <118715150+didwldk@users.noreply.github.com>
2023-09-12 19:18:11 +0900

    안드로이드 프로젝트 Full_Moon 생성 및 협력 연동

---
+++ app/src/main/java/com/cookandroid/android_pj/Start_screen.java
@@ -0,0 +1,14 @@
+package com.cookandroid.android_pj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Start_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+}

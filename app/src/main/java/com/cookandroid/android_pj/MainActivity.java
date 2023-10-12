232e0b5521987526c88fbc806785a90b75af3c39
gitdoyeon <gitpdyeon@gmail.com>
2023-10-12 17:11:47 +0900

    화면 넘어가기

--- app/src/main/java/com/cookandroid/android_pj/MainActivity.java
+++ app/src/main/java/com/cookandroid/android_pj/MainActivity.java
@@ -2,13 +2,55 @@ package com.cookandroid.android_pj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button photo_btn, title_btn, start_btn;
+    EditText title_edt, max_edt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start_screen);
+        setTitle("시작 설정 화면");
+
+        photo_btn = findViewById(R.id.photo_btn);
+        title_btn = findViewById(R.id.title_btn);
+        start_btn = findViewById(R.id.start_btn);
+        title_edt = findViewById(R.id.title_edt);
+        max_edt = findViewById(R.id.max_edt);
+
+        // 저금통 사진 넣고 완료
+        photo_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // 저금통 제목 넣고 완료
+        title_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // 저금통 시작하기 버튼 - 누르면 activity_main.xml로 넘어감
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Start_screen.class);  // 화면 전환
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
--- app/src/main/java/com/cookandroid/android_pj/Start_screen.java
+++ app/src/main/java/com/cookandroid/android_pj/Start_screen.java
@@ -2,13 +2,40 @@ package com.cookandroid.android_pj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Start_screen extends AppCompatActivity {
+    Button push_btn, pull_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_screen);
+        setContentView(R.layout.activity_main);
+        setTitle("저금통");
+
+        push_btn = findViewById(R.id.push_btn);
+        pull_btn = findViewById(R.id.pull_btn);
+
+//        채우기 -> 채우는 화면 이동(Transcation)
+        push_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Start_screen.this, Transaction.class);   // 화면 전환
+                startActivity(intent);
+            }
+        });
+
+//        꺼내기 -> 꺼내는 화면 이동(Transcation)
+        push_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Start_screen.this, Transaction.class);   // 화면 전환
+                startActivity(intent);
+            }
+        });
+
     }
 }

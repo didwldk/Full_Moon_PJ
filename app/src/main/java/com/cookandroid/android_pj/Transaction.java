ed7e395ef51a49b2d93992b0182bd992894371ae
didwldk <118715150+didwldk@users.noreply.github.com>
2023-11-07 19:54:01 +0900

    자바 메모 기능 시작

--- app/src/main/java/com/cookandroid/android_pj/Transaction.java
+++ app/src/main/java/com/cookandroid/android_pj/Transaction.java
@@ -2,19 +2,35 @@ package com.cookandroid.android_pj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
 
-public class Transaction extends AppCompatActivity {
+
+
+import java.util.ArrayList;
+
+public class Transaction<AddActivity> extends AppCompatActivity {
+    private static String TAG = "MainActivity";
+    Button load, save;
+    EditText inputText;
+
+
     Button back_btn;
     TextView title_txt;
+    TextView textView;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction);
 
@@ -25,8 +41,12 @@ public class Transaction extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(Transaction.this, Start_screen.class);  // 화면 전환
                 startActivity(intent);
+
             }
+
+
         });
+
 
     }
 
